aca58c9833caa2008cff1e706f3da52e7f8cfb5c
Statharos332 <stathispapounidis@gmail.com>
2026-05-27 13:59:04 +0200

    structure sanity change

--- sanity/sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import project from './project'
-import fragment from './fragment'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-    types: [project, fragment],
-}
